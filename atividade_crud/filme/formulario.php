c92ac36964e60ae8759bbe447820cdc43527215c
Camila <camila.facanali@alunos.ifsuldeminas.edu.br>
2023-06-19 08:33:36 -0300

    deusamor

--- atividade_crud/filme/formulario.php
+++ atividade_crud/filme/formulario.php
@@ -12,17 +12,19 @@
      "inserir.php"; ?>"
       method="post" enctype="multipart/form-data">
         
+      <input type="hidden" name="codigo" value="<?php echo isset($filme) ? $filme->codigo : '' ?>" ><br>
+
         <label>Título</label><br>
-        <input type="text" name="titulo" value=""> <br>
+        <input type="text" name="titulo" value="<?php echo isset($filme) ? $filme->titulo : '' ;?>" > <br>
 
         <label>Sinopse</label><br>
-        <input type="text" name="sinopse" value=""> <br>
+        <input type="text" name="sinopse" value="<?php echo isset($filme) ? $filme->sinopse : '' ?>" > <br>
 
         <label>Ano de lançamento</label><br>
-        <input type="text" name="ano" value=""> <br>
+        <input type="text" name="ano" value="<?php echo isset($filme) ? $filme->ano : '' ?>" > <br>
 
         <label>Diretor</label><br>
-        <input type="text" name="diretor" value=""> <br> 
+        <input type="text" name="diretor" value="<?php echo isset($filme) ? $filme->diretor : '' ?>" > <br> 
 
         <br>
         <button type="submit">Inserir</button>
